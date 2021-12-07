66672827364718b3c6af012bde0e763453e8489b
y <gyk9704@gmail.com>
2021-12-07 21:08:37 +0900

    1207

--- 2_basic/1_matrix-decomposition.py
+++ 2_basic/1_matrix-decomposition.py
@@ -1,1 +1,14 @@
 # Matrix Decomposition
+import numpy as np
+# 원본 행렬 R 생성, 분해 행렬 P와 Q 초기화, 잠재요인 차원 K는 3 설정.
+R = np.array([[4, np.NaN, np.NaN, 2, np.NaN ],
+              [np.NaN, 5, np.NaN, 3, 1 ],
+              [np.NaN, np.NaN, 3, 4, 4 ],
+              [5, 2, 1, 2, np.NaN ]])
+num_users, num_items = R.shape
+K=3
+# P와 Q 매트릭스의 크기를 지정하고 정규분포를 가진 random한 값으로 입력합니다.
+np.random.seed(1)
+P = np.random.normal(scale=1./K, size=(num_users, K))
+Q = np.random.normal(scale=1./K, size=(num_items, K))
+print('실제 행렬:\n', R)
